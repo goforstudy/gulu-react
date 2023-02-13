d108efb3ef58e2d1c58ebbcb98f83530847792ac
wangtao <wat0x01@sp.noahgroup.com>
2023-02-13 20:59:13 +0800

    设置不同环境的webpack

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-    mode: 'production',
+    // mode: 'production',
     entry: {
         index: './lib/index.tsx'
     },
@@ -21,24 +21,4 @@ module.exports = {
             }
         ]
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'GR',
-            template: 'index.html'
-        })
-    ],
-    // externals: {
-    //     react: {
-    //         commonjs: "react",
-    //         commonjs2: "react",
-    //         amd: "react",
-    //         root: "React"
-    //     },
-    //     'react-dom': {
-    //         commonjs: "react-dom",
-    //         commonjs2: "react-dom",
-    //         amd: "react-dom",
-    //         root: "ReactDOM"
-    //     }
-    // }
 }
